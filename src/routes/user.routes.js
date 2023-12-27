ada3ab6e63ffc60b80ddbb9a0123be7c08f7fb08
Shubhamkrjha827 <shubhamkumarjha827@gmail.com>
2023-12-27 05:45:01 -0800

    add pipelines getwatch history
    Signed-off-by: Shubhamkrjha827 <shubhamkumarjha827@gmail.com>

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refereshAcessToken } from "../controllers/user.controller.js";
+import { 
+   loginUser, 
+   logoutUser, 
+   registerUser, 
+   refereshAcessToken,
+   changeCurrentPassword, 
+   getCurrentUser, 
+   updateAccountDetails,
+   updateUserAvatar,
+   updateUserCoverImage,
+   getUserChannelProfile,
+   getWatchHistory
+
+} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -25,6 +38,16 @@ router.route("/register").post(
     //secured routes
     router.route("/logout").post(verifyJWT, logoutUser)
     router.route("/referesh-token").post(refereshAcessToken)
+
+    router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+    router.route("/current-user").get(verifyJWT, getCurrentUser)
+    router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+   router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+    router.route("/cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+
+   router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+   router.route("/history").get(verifyJWT, getWatchHistory)
 
 
     
